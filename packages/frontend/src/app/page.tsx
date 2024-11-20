ee08b14dffdc836c000b4118a562ead9e2c3fde0
Paul Nassar <pfnassar@calpoly.edu>
2024-11-20 11:01:20 -0800

    implemented superficial settings pages

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -9,13 +9,16 @@ import { WeightPlot } from "@/components/weight-plot";
 import { DateAggregation } from "@/components/date-aggreation";
 import { Button } from "@/components/ui/button";
 import { LoginModal } from "@/components/login-modal";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex space-x-4">
         <ModeToggle />
-        <Button>Log In</Button>
+        <Link href="/account">
+          <Button>My Account</Button>
+        </Link>
       </div>
       <div className="space-y-4">
         <h1 className="text-2xl font-bold">Daily Calories</h1>
