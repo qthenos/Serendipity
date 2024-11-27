579c981f0d5036cbbfbb945a52d922d9ada4bf20
Dakshesh <dpasala@calpoly.edu>
2024-11-27 14:19:34 -0800

    Switching to diff branch

---
+++ packages/frontend/dist/route.js
@@ -0,0 +1,86 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '/Users/dakshesh/Serendipity/packages/frontend/.env' });
+console.log("EDAMAM_APP_ID:", process.env.EDAMAM_APP_ID);
+console.log("EDAMAM_APP_KEY:", process.env.EDAMAM_APP_KEY);
+var EDAMAM_APP_ID = process.env.EDAMAM_APP_ID;
+var EDAMAM_APP_KEY = process.env.EDAMAM_APP_KEY;
+var url = 'https://api.edamam.com/api/food-database/v2/parser';
+console.log("API Request URL: ".concat(url, "?app_id=").concat(EDAMAM_APP_ID, "&app_key=").concat(EDAMAM_APP_KEY, "&ingr=apple"));
+if (!EDAMAM_APP_ID || !EDAMAM_APP_KEY) {
+    throw new Error("Missing API credentials. Please check your .env file.");
+}
+function testAPI(query) {
+    return __awaiter(this, void 0, void 0, function () {
+        var url, response, error_1;
+        var _a;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    url = 'https://api.edamam.com/api/food-database/v2/parser';
+                    _b.label = 1;
+                case 1:
+                    _b.trys.push([1, 3, , 4]);
+                    return [4 /*yield*/, axios.get(url, {
+                            params: {
+                                app_id: EDAMAM_APP_ID,
+                                app_key: EDAMAM_APP_KEY,
+                                ingr: query,
+                            },
+                        })];
+                case 2:
+                    response = _b.sent();
+                    console.log("Response Data:", response.data);
+                    console.log("First Item:", (_a = response.data.hints[0]) === null || _a === void 0 ? void 0 : _a.food);
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_1 = _b.sent();
+                    if (error_1 instanceof Error) {
+                        console.error("Error making API request:", error_1.message);
+                    }
+                    else {
+                        console.error("An unknown error occurred");
+                    }
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+testAPI('chocolate');
