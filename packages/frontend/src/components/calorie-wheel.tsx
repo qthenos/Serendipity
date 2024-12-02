cf335a59841e224407c02af72cd8cd2656c6879a
James Irwin <jamesirwin0211@gmail.com>
2024-12-02 11:31:00 -0800

    dynamic date

--- packages/frontend/src/components/calorie-wheel.tsx
+++ packages/frontend/src/components/calorie-wheel.tsx
@@ -66,7 +66,7 @@ export function CalorieWheel() {
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
         <CardTitle>Calorie Breakdown</CardTitle>
-        <CardDescription>October 23, 2024</CardDescription>
+        <CardDescription>$[formattedDate]</CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
