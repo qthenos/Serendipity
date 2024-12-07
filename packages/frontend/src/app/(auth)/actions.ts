262df1e0d96e27dd0e8fedecfaceff6d5f8a094f
Sahith <sahith.karra@gmail.com>
2024-12-06 18:07:19 -0800

    signupo toast message

--- packages/frontend/src/app/(auth)/actions.ts
+++ packages/frontend/src/app/(auth)/actions.ts
@@ -28,8 +28,6 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
   }
@@ -38,7 +36,7 @@ export async function signup(formData: FormData) {
     email: data.email,
     options: {
       shouldCreateUser: false,
-      emailRedirectTo: '/', // Redirect to the root path
+      emailRedirectTo: '/',
     },
   })
 
